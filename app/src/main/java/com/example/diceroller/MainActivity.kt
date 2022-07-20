976a85eb3aeb4b83edbc09bcf25dc9acff0ab8b8
Sandeep-Kaur-GitHub <kaur94sandeep@gmail.com>
2022-07-20 14:25:46 +0530

    Add different images of dices on button click

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -11,17 +11,26 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.button.setOnClickListener(View.OnClickListener {
-            //Toast.makeText(applicationContext,"this is toast message",Toast.LENGTH_SHORT).show()
+        val btn = binding.button
+        btn.setOnClickListener(View.OnClickListener {
             rollDice()
         })
-
     }
 
     private fun rollDice() {
-        val random= Random.nextInt(6)
-        binding.textview.setText(random.toString())
+        val img = binding.dice
+        val random = Random.nextInt(6) + 1
+        var drawableResource = when (random) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        img.setImageResource(drawableResource)
     }
 }
